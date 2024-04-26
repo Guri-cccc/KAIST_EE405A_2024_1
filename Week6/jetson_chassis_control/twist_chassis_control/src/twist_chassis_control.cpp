2dc1b2eaa9f8b2432721053eaa94e8df40fc3cae
MinSungjae <msj5826@gmail.com>
2024-04-26 13:07:06 +0900

    Full twist_chassis_control

--- Week6/jetson_chassis_control/twist_chassis_control/src/twist_chassis_control.cpp
+++ Week6/jetson_chassis_control/twist_chassis_control/src/twist_chassis_control.cpp
@@ -4,15 +4,13 @@ TwistChassisControl::TwistChassisControl(ros::NodeHandle* nh)
 {
     _nh = nh;
 
-    // TODO
-    _set_velocity_pub = _nh->advertise<chassis_control::SetVelocity>("", 1, this);
-    _twist_cmd_sub = _nh->subscribe("", 1, , this);
+    _set_velocity_pub = _nh->advertise<chassis_control::SetVelocity>("chassis_control/set_velocity", 1, this);
+    _twist_cmd_sub = _nh->subscribe("cmd_vel", 1, &TwistChassisControl::_twist_cmd_cb, this);
 }
 
 void TwistChassisControl::_twist_cmd_cb(const geometry_msgs::Twist& msg)
 {
-    // TODO
-    _twist_updated_time = ;
+    _twist_updated_time = ros::Time::now();
     _twist_cmd = msg;
 }
 
@@ -25,19 +23,18 @@ bool TwistChassisControl::control()
         _set_velocity_pub.publish(empty_cmd);    
         return false;
     }
+    
+    double vx = _twist_cmd.linear.x;
+    double vy = _twist_cmd.linear.y;
+    double wz = _twist_cmd.angular.z;
 
-    // TODO
-    double vx = 0.0;
-    double vy = 0.0;
-    double wz = 0.0;
+    // SetVelocity.velocity : [mm/s], Twist : [m/s]
+    double speed = sqrt(pow(vx,2)+pow(vy,2)) * 1000.0;
 
-    // Hint: SetVelocity.velocity : [mm/s], Twist : [m/s]
-    double speed = 0.0;
+    // SetVelocity.direction : -Vy
+    double heading = R2D(atan2(vy, vx)) + 90.0;
 
-    // Hint: SetVelocity.direction 0 dgree: : -Vy
-    double heading = 0.0;
-
-    double angular = 0.0;
+    double angular = wz;
 
     _velocity_cmd.velocity = speed;
     _velocity_cmd.direction = heading;
